33393257d9052370eda2863cdb4250e673de7721
Juan Pablo Ballistreri <jpballistreri@gmail.com>
2022-11-16 01:12:51 -0300

    Fix checkJWT

--- api/src/middleware/checkJWT.js
+++ api/src/middleware/checkJWT.js
@@ -2,15 +2,17 @@ const jwt = require("jsonwebtoken");
 
 const checkJWT = (req, res, next) => {
   if (req.headers.cookie) {
-    const token = req.headers.cookie.split(";")[1].split("=")[1];
     try {
+      const cookies = req.headers.cookie.split(" ");
+      let token = cookies.find((cookie) => cookie.substring(0, 4) == "jwt=");
+      token = token.split("=")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
       decoded && next();
     } catch (error) {
-      res.status(403).send("Unauthorized");
+      res.status(403).send({ message: "Unauthorized" });
     }
   } else {
-    res.status(403).send("Unauthorized");
+    res.status(403).send({ message: "Unauthorized" });
   }
 };
 
